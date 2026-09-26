6d332c0d21dbb2334215f954fe985270569e6670
Vaibhavtech2006 <vaibhavkhandelwal2006@gmail.com>
2026-09-27 02:43:31 +0530

    add requirements

--- main_server.py
+++ main_server.py
@@ -1,3 +1,12 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 import os
 import re
 from dotenv import load_dotenv
